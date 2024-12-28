6c252818b3e32b5ef1e197b2a94a0aa22978b34c
Patrice CAVEZZAN <pcavezzan@gmail.com>
2024-12-28 21:51:30 +0100

    Day 7 - Bridge Repair

--- 2024/07/puzzle/game.v
+++ 2024/07/puzzle/game.v
@@ -24,3 +24,32 @@ pub fn from_string(content string) Game {
 	}
 	return Game{equations}
 }
+
+pub fn (game Game) solve_equations() int {
+	mut result := 0
+	for equation in game.equations {
+		mut time := equation.numbers[0]
+		mut sum := equation.numbers[0]
+		mut mix_start_with_time := equation.numbers[0]
+		mut mix_start_with_sum := equation.numbers[0]
+
+		for i := 1; i < equation.numbers.len; i++ {
+			time *= equation.numbers[i]
+			sum += equation.numbers[i]
+			if i % 2 == 0 {
+				mix_start_with_sum *= equation.numbers[i]
+				mix_start_with_time += equation.numbers[i]
+			} else {
+				mix_start_with_sum += equation.numbers[i]
+				mix_start_with_time *= equation.numbers[i]
+			}
+		}
+
+		if equation.test_value == time || equation.test_value == sum
+			|| equation.test_value == mix_start_with_time
+			|| equation.test_value == mix_start_with_sum {
+			result += equation.test_value
+		}
+	}
+	return result
+}
--- 2024/07/puzzle/game_test.v
+++ 2024/07/puzzle/game_test.v
@@ -12,6 +12,79 @@ fn test_new_instance() {
 	assert game.equations[1].numbers == [81, 40, 27]
 }
 
-fn test_equations_can_be_made() {
-	assert true == false
+fn test_equations_can_be_made_when_test_value_is_numbers_times() {
+	game := Game{
+		equations: [
+			Equation{
+				test_value: 190
+				numbers:    [10, 19]
+			},
+		]
+	}
+
+	equations := game.solve_equations()
+
+	assert equations == 190
+}
+
+fn test_equations_can_be_made_when_test_value_is_numbers_times_with_sum() {
+	game := Game{
+		equations: [
+			Equation{
+				test_value: 3267
+				numbers:    [81, 40, 27]
+			},
+		]
+	}
+
+	equations := game.solve_equations()
+
+	assert equations == 3267
+}
+
+fn test_solve_equations_return_sum_of_equations_that_could_be_made() {
+	game := Game{
+		equations: [
+			Equation{
+				test_value: 190
+				numbers:    [10, 19]
+			},
+			Equation{
+				test_value: 3267
+				numbers:    [81, 40, 27]
+			},
+			Equation{
+				test_value: 83
+				numbers:    [17, 5]
+			},
+			Equation{
+				test_value: 156
+				numbers:    [15, 6]
+			},
+			Equation{
+				test_value: 7290
+				numbers:    [6, 8, 6, 15]
+			},
+			Equation{
+				test_value: 161011
+				numbers:    [16, 10, 13]
+			},
+			Equation{
+				test_value: 192
+				numbers:    [17, 8, 14]
+			},
+			Equation{
+				test_value: 21037
+				numbers:    [9, 7, 18, 13]
+			},
+			Equation{
+				test_value: 292
+				numbers:    [11, 6, 16, 20]
+			},
+		]
+	}
+
+	equations := game.solve_equations()
+
+	assert equations == 3749
 }
