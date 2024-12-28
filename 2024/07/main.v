7270cb3d2ee526d53b29ec8eb3968d643fb1c1ce
Patrice CAVEZZAN <pcavezzan@gmail.com>
2024-12-28 21:53:05 +0100

    Day 7 - Bridge Repair

--- 2024/07/main.v
+++ 2024/07/main.v
@@ -1,8 +1,12 @@
 module main
 
 import os
+import puzzle
 
 fn main() {
 	input_file_content := os.read_file('./input.txt')!
 	println(input_file_content)
+	game := puzzle.from_string(input_file_content)
+	total_calibration_result := game.solve_equations()
+	println('total calibration result: ${total_calibration_result}')
 }
