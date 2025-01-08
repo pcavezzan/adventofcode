4fbf75e59d9344a2a3f18e707ff2253385de5f76
Patrice CAVEZZAN <pcavezzan@gmail.com>
2025-01-09 00:44:40 +0100

    Day 9: Disk Fragmenter.
    Refactoring: removing useless enum.

--- 2024/09/diskmap/diskmap.v
+++ 2024/09/diskmap/diskmap.v
@@ -6,15 +6,9 @@ struct IndividualBlock {
 	value int
 }
 
-enum BlockType {
-	file
-	free_space
-}
-
 const empty_id_value = -1
 
 struct Block {
-	bloc_type       BlockType
 	number_of_blocs int
 	id              int = empty_id_value
 }
@@ -22,14 +16,12 @@ struct Block {
 fn new_file_block(id int, block int) Block {
 	return Block{
 		id:              id
-		bloc_type:       BlockType.file
 		number_of_blocs: block
 	}
 }
 
 fn new_free_space_block(block int) Block {
 	return Block{
-		bloc_type:       BlockType.free_space
 		number_of_blocs: block
 	}
 }
--- 2024/09/diskmap/diskmap_test.v
+++ 2024/09/diskmap/diskmap_test.v
@@ -8,27 +8,22 @@ fn test_create_diskmap_from_input() {
 	assert disk_map == DiskMap{
 		blocs: [
 			Block{
-				bloc_type:       .file
 				number_of_blocs: 1
 				id:              0
 			},
 			Block{
-				bloc_type:       .free_space
 				number_of_blocs: 2
 				id:              -1
 			},
 			Block{
-				bloc_type:       .file
 				number_of_blocs: 3
 				id:              1
 			},
 			Block{
-				bloc_type:       .free_space
 				number_of_blocs: 4
 				id:              -1
 			},
 			Block{
-				bloc_type:       .file
 				number_of_blocs: 5
 				id:              2
 			},
