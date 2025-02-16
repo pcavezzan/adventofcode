c7c765752c12c71c19de9b0df162f8c9380157cd
Patrice CAVEZZAN <pcavezzan@gmail.com>
2025-02-17 00:47:24 +0100

    WIP

--- 2024/10/topographic/map.v
+++ 2024/10/topographic/map.v
@@ -1,6 +1,8 @@
 module topographic
 
 struct Position {
+	x      int
+	y      int
 	height int
 }
 
@@ -11,13 +13,15 @@ struct Map {
 pub fn new_map_from_puzzle(puzzle string) &Map {
 	lines := puzzle.split_into_lines()
 	mut positions := [][]Position{len: 0, cap: lines.len}
-	for line in lines {
+	for row_number, line in lines {
 		runes := line.runes()
 		mut row := []Position{len: 0, cap: runes.len}
-		for c in runes {
+		for column, c in runes {
 			height_value := c.str().int()
 			row << Position{
 				height: height_value
+				x:      column
+				y:      row_number
 			}
 		}
 		positions << row
@@ -28,9 +32,35 @@ pub fn new_map_from_puzzle(puzzle string) &Map {
 }
 
 struct TrailHead {
-	visited []int
+	visited []Position
 }
 
 fn (m Map) find_hiking_trails() TrailHead {
+	mut next_pos := Position{}
+	mut start := false
+	for row, row_positions in m.positions {
+		for column, pos in row_positions {
+			//
+			if pos.height == 1 {
+				next_pos = pos
+				start = true
+				break
+			}
+		}
+	}
+
+	if !start {
+		return TrailHead{}
+	}
+
+	counter := 0
+	for {
+		next_height := next_pos.height
+
+		if counter == 10 {
+			break
+		}
+	}
+
 	return TrailHead{}
 }
